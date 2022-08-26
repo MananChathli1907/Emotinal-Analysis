b83bb69006b1584647d56ae6a38b829e61a12fdc
MananChathli1907 <74950124+MananChathli1907@users.noreply.github.com>
2022-08-26 23:38:55 +0530

    Update more.py

---
+++ Python-Files/more.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def more():
+    st.markdown("<h1 style='text-align: left; color: white;'></h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: left; color: white;'>Algorithm Used : </h3>", unsafe_allow_html=True)
+
+    st.write("➔ sklearn Pipeline [Learn more about Pipelines](https://scikit-learn.org/stable/modules/generated/sklearn.pipeline.Pipeline.html)")
+    st.write("➔ sklearn Count Vectorizer[Learn more about Count Vectorizer](https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html)")
+    st.write("➔ sklearn Logistic Regression [Learn more about Logistic Regression](https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html)")
+    st.write("Github Link: (https://github.com/MananChathli1907/Emotinal-Analysis.git)")
+
+    st.markdown("<h1 style='text-align: left; color: white;'></h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: left; color: white;'>About the author : </h3>", unsafe_allow_html=True)
+
+    st.markdown("<h5 style='text-align: left; color: white;'>Name : Manan Chathli</h5>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; color: white;'>Age : 19</h5>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; color: white;'>Designation : Student at Amity University Noida</h5>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; color: white;'>Email : manan.chathli@s.amity.edu</h5>", unsafe_allow_html=True)
+    st.write("LinkedIn Profile: (http://linkedin.com/in/manan-chathli-5aa174241)")
